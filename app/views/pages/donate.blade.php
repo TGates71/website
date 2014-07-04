e5c94df08d4cc69a2c0e284d2ac2485b543885ff
Bobby Allen <ballen@bobbyallen.me>
2014-07-04 11:56:44 +0100

    Donations page 90% complete, just need to add PayPal donate button code, will finish this after I've registered a new PayPal address under the Sentora project name.

--- app/views/pages/donate.blade.php
+++ app/views/pages/donate.blade.php
@@ -27,11 +27,27 @@ Donate - Sentora
     </div>
 </section>
 <div class="divied-60"></div>
-
 <div class="container">
     <div class="row">
+        <div class="col-md-6">
+            <h3>Make a donation</h3>
+            <p>Please help drive the project forward and showing your appreciation to the team but donating some money via. PayPal</p>
+            <p>All donations received go towards paying our on-going running costs such as hosting, domain names and where applicable software licenses etc.</p>
+            <p>We truly appreciate all donations, if you use Sentora and appreciate the hard work that our team puts into the project please help us by donating.</p>
+            <p>Many thanks,<br/>The Sentora Team</p>
 
+        </div>
+        <div class="col-md-6">
+            <h3>With thanks to...</h3>
+            <p>We would like to personally thank the following list of people/organisations of whom have made a larger than normal (&dollar;100+) donation to the project as well as those of whom have donated smaller amounts or wished to remain anonymous.</p>
+            <ul>
+                <li><a href="http://www.supared.com/?ref=sentora" target="_blank">Supared</a></li>
+                <li><a href="http://www.zvps.co.uk/?ref=sentora" target="_blank">zVPS</a></li>
+                <li><a href="http://www.mach-hosting.com/?ref=sentora target="_blank">Mach Hosting</a></li>  
+            </ul>
+        </div>
     </div>
+    <div class="divied-40"></div>
 </div>
 @stop
 
