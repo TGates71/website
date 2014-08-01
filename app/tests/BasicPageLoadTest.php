0743de6756d5b517017b4b9c94cfdba996c313b6
Bobby Allen <ballen@bobbyallen.me>
2014-08-01 15:47:43 +0100

    Added new unit tests for simple page and standard page load error capturing, will add future unit tests for the API requests etc. over the weekend.

---
+++ app/tests/BasicPageLoadTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * A simple set of unit tests to quickly catch any 'normal' errors eg. 500,
+ *  404 etc
+ */
+class BasicPageLoadTest extends TestCase
+{
+
+    /**
+     * Makes a request to the 'homepage' and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testHomePageIsOk()
+    {
+        $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * Makes a request to the 'About' page and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testAboutPageIsOk()
+    {
+        $this->client->request('GET', '/about');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * Makes a request to the 'The team' page and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testTheTeamPageIsOk()
+    {
+        $this->client->request('GET', '/about/the-team');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * Makes a request to the 'Donate' page and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testDonatePageIsOk()
+    {
+        $this->client->request('GET', '/donate');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * Makes a request to the 'Downloads' page and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testDownloadPageIsOk()
+    {
+        $this->client->request('GET', '/download');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * Makes a request to the 'Support' page and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testSupportPageIsOk()
+    {
+        $this->client->request('GET', '/support');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    /**
+     * Makes a request to the 'FAQs' page and checks that it gets
+     * redirected to the docs site.
+     */
+    public function testFaqsPageIsRedirected()
+    {
+        $this->client->request('GET', '/support/faqs');
+        $this->assertTrue($this->client->getResponse()->isRedirect('http://docs.sentora.io/?node=94'));
+    }
+
+    /**
+     * Makes a request to the XML sitemap and checks that is returns a 
+     * simple status 200 response code.
+     */
+    public function testSiteMapPageIsOk()
+    {
+        $this->client->request('GET', '/sitemap.xml');
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+}
